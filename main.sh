9103414e8febb1419cfbfc3958055e9a8f4d3563
b1bab0ba <kuzmin2002@mail.ru>
2021-12-17 16:55:19 +0300

    Errors fixed. Readability increase. All constuctions is commented.

--- main.sh
+++ main.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-#The following parametrs can be changed
-countOfPhotos=(1 2 3 4 5 6 7 8 9 10)
-sleepTime=0
-resolution="1920x1080"
-frameToSkip=20
-isSettingUpNeeded=1
+# The following parametrs can be changed
 
-# Common path for all GPIO access
+# Count of photos to take
+countOfPhotos=(1 2 3 4 5 6 7 8 9 10)
+
+# Time to wait before camera focused, adapted and etc.
+cameraSleepTime=0
+
+# Time to wait before/after lights are on
+lightSleepTime=1
+# Camera resolution
+resolution="1920x1080"
+
+# If camera has auto-brigthness, this parametr needed to be at least 20
+frameToSkip=20
+
+# Needed to be 1 if script will be run at the first time
+isSettingUpNeeded=0
+
+# Common path for all GPIO access. DO NOT CHANGE IF YOU HAVE STOCK RASPBERRY PI OS!
 BASE_GPIO_PATH=/sys/class/gpio
 
-# Assign names to GPIO pin numbers for each light
+# Assign names to GPIO pin numbers for each light.
 PIN_1=9
 
 # Assign names to states
 ON="1"
-OFF="2"
+OFF="0"
 
+# Function to export pin to GPIO path
 exportPin()
 {
   if [ ! -e $BASE_GPIO_PATH/gpio$1 ]; then
@@ -24,19 +37,19 @@ exportPin()
   fi
 }
 
-# Utility function to set a pin as an output
+# Function to set a pin as an output
 setOutput()
 {
   echo "out" > $BASE_GPIO_PATH/gpio$1/direction
 }
 
-# Utility function to change state of a light
+# Function to change state of a light
 setLightState()
 {
   echo $2 > $BASE_GPIO_PATH/gpio$1/value
 }
 
-# Utility function to turn all lights off
+# Function to turn all lights off
 allLightsOff()
 {
   setLightState $PIN_1 $OFF
@@ -49,7 +62,7 @@ shutdown()
   exit 0
 }
 
-trap shutdown SIGINT
+#trap shutdown SIGINT
 
 # Export pins so that we can use them
 exportPin $PIN_1
@@ -60,8 +73,8 @@ setOutput $PIN_1
 # Turn lights off to begin
 allLightsOff
 
-
-
+# Function to prepare for run main part of script
+# This function include installations, creating folders, tests & etc
 settingUp(){
 	sudo apt update
 	sudo apt install fswebcam
@@ -70,23 +83,26 @@ settingUp(){
 	cd project_photos
 }
 
+# Function to take one formatted picture
 takePhoto() {
 	fswebcam --line-colour \#FFFFFFFF --banner-colour \#FFFFFFFF --timestamp "$i photo %Y-%m-%d %H:%M:%S" $i
-	sleep $sleepTime
+	sleep $cameraSleepTime
 }
 
-if [[isSettingUpNeeded]]
+# Checking for setting up condition
+if [ $isSettingUpNeeded -eq 1 ]
 then
 	settingUp
 fi
-# Loop forever until user presses Ctrl-C
 
+# Main loop
 for i in ${countOfPhotos[*]}
 do
   setLightState $PIN_1 $ON
-  sleep 2
+  sleep $lightSleepTime
   takePhoto
   setLightState $PIN_1 $OFF
+  sleep $lightSleepTime
 
 done
 
