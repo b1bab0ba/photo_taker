bad22de80d319ca0248ba56e39e0c9a0ef9cdaa8
b1bab0ba <kuzmin2002@mail.ru>
2021-12-17 10:57:03 +0300

    GPIO interactions. Added: 1.GPIO 1 pin initialization 2.Functions for GPIO control 3.Setting up function

--- main.sh
+++ main.sh
@@ -1,23 +1,99 @@
 #!/bin/bash
 
+#The following parametrs can be changed
+countOfPhotos=(1 2 3 4 5 6 7 8 9 10)
+sleepTime=0
+resolution="1920x1080"
+frameToSkip=20
+isSettingUpNeeded=1
+
+# Common path for all GPIO access
+BASE_GPIO_PATH=/sys/class/gpio
+
+# Assign names to GPIO pin numbers for each light
+PIN_1=9
+
+# Assign names to states
+ON="1"
+OFF="2"
+
+exportPin()
+{
+  if [ ! -e $BASE_GPIO_PATH/gpio$1 ]; then
+    echo "$1" > $BASE_GPIO_PATH/export
+  fi
+}
+
+# Utility function to set a pin as an output
+setOutput()
+{
+  echo "out" > $BASE_GPIO_PATH/gpio$1/direction
+}
+
+# Utility function to change state of a light
+setLightState()
+{
+  echo $2 > $BASE_GPIO_PATH/gpio$1/value
+}
+
+# Utility function to turn all lights off
+allLightsOff()
+{
+  setLightState $RED $OFF
+  setLightState $YELLOW $OFF
+  setLightState $GREEN $OFF
+}
+
+# Ctrl-C handler for clean shutdown
+shutdown()
+{
+  allLightsOff
+  exit 0
+}
+
+trap shutdown SIGINT
+
+# Export pins so that we can use them
+exportPin $RED
+exportPin $YELLOW
+exportPin $GREEN
+
+# Set pins as outputs
+setOutput $RED
+setOutput $YELLOW
+setOutput $GREEN
+
+# Turn lights off to begin
+allLightsOff
+
+
+
+settingUp(){
+	sudo apt update
+	sudo apt install fswebcam
+	cd ~
+	mkdir project_photos
+	cd project_photos
+}
+
 takePhoto() {
 	fswebcam --line-colour \#FFFFFFFF --banner-colour \#FFFFFFFF --timestamp "$i photo %Y-%m-%d %H:%M:%S" $i
 	sleep $sleepTime
 }
 
-#sudo apt update
-#sudo apt install fswebcam
-
-countOfPhotos=(1 2 3 4 5 6 7 8 9 10)
-sleepTime=0
-
-cd ~
-mkdir project_photos
-cd project_photos
+if [[isSettingUpNeeded]]
+then
+	settingUp
+fi
+# Loop forever until user presses Ctrl-C
 
 for i in ${countOfPhotos[*]}
 do
-	takePhoto
+  setLightState $PIN_1 $ON
+  sleep 2
+  takePhoto
+  setLightState $PIN_1 $OFF
+
 done
 
 
