2bd9268054621a5e630bea4c36f24113a3bdbfde
b1bab0ba <kuzmin2002@mail.ru>
2021-12-18 00:07:04 +0300

    Version 1.0. Added all 10 output pins, 1 input pin for button control

--- main.sh
+++ main.sh
@@ -25,8 +25,21 @@ dirToSave=project_photos
 BASE_GPIO_PATH=/sys/class/gpio
 
 # Assign names to GPIO pin numbers for each light.
-PIN_1=9
+PIN_1=5
+PIN_2=11
+PIN_3=9
+PIN_4=10
+PIN_5=22
+PIN_6=27
+PIN_7=17
+PIN_8=4
+PIN_9=3
+PIN_10=2
 
+listOfPins=($PIN_1 $PIN_2 $PIN_3 $PIN_4 $PIN_5 $PIN_6 $PIN_7 $PIN_8 $PIN_9 $PIN_10) 
+
+# Assign name to input GPIO pin for button
+BUTTON=18
 # Assign names to states
 ON="1"
 OFF="0"
@@ -34,34 +47,62 @@ OFF="0"
 # Function to export pin to GPIO path
 exportPin()
 {
-  if [ ! -e $BASE_GPIO_PATH/gpio$1 ]; then
-    echo "$1" > $BASE_GPIO_PATH/export
-  fi
+  	if [ ! -e $BASE_GPIO_PATH/gpio$1 ]; then
+   		sudo echo "$1" > $BASE_GPIO_PATH/export
+  	fi
 }
 
 # Function to set a pin as an output
 setOutput()
 {
-  echo "out" > $BASE_GPIO_PATH/gpio$1/direction
+  	sudo echo "out" > $BASE_GPIO_PATH/gpio$1/direction
+}
+
+# Function to set a pin as an input
+setInput()
+{
+	sudo echo "in" > $BASE_GPIO_PATH/gpio$1/direction
 }
 
 # Function to change state of a light
 setLightState()
 {
-  echo $2 > $BASE_GPIO_PATH/gpio$1/value
+  	sudo echo $2 > $BASE_GPIO_PATH/gpio$1/value
+}
+
+# Function to get value from input pin
+getValue()
+{
+	sudo cat $BASE_GPIO_PATH/gpio$1/value
+	
 }
 
 # Function to turn all lights off
 allLightsOff()
 {
-  setLightState $PIN_1 $OFF
+	for i in ${listOfPins[*]}
+	do	
+		setLightState $i $OFF
+	done
 }
 
-# Export pins so that we can use them
-exportPin $PIN_1
+# Export output pins
+for i in ${listOfPins[*]}
+do	
+	exportPin $i
+done
+
+# Export input pins 
+exportPin $BUTTON
 
 # Set pins as outputs
-setOutput $PIN_1
+for i in ${listOfPins[*]}
+do	
+	setOutput $i
+done
+
+# Set pins as inputs
+setInput $BUTTON
 
 # Turn lights off to begin
 allLightsOff
@@ -69,18 +110,18 @@ allLightsOff
 # Function to prepare for run main part of script
 # This function include installations, creating folders, tests & etc
 settingUp(){
-  sudo apt update
-  sudo apt install fswebcam
-  isDirExist='find ~ -type d -name "$dirToSave"'
-  if [ -z isDirExist  && $? -eq 0 ]
-  then
-       mkdir ~/$dirToSave
-  fi
+  	sudo apt update
+  	sudo apt install fswebcam
+  	isDirExist='find ~ -type d -name "$dirToSave"'
+  	if [ -z isDirExist  && $? -eq 0 ]
+	then
+       	mkdir ~/$dirToSave
+  	fi
 } 
 
 # Function to take one formatted picture
 takePhoto() {
-	fswebcam --line-colour \#FFFFFFFF --banner-colour \#FFFFFFFF --timestamp "$i photo %Y-%m-%d %H:%M:%S" -r $resolution -S $framesToSkip --font sans:20 $i
+	fswebcam --line-colour \#FFFFFFFF --banner-colour \#FFFFFFFF --timestamp "$(( $i + 1 )) photo %Y-%m-%d %H:%M:%S" -r $resolution -S $framesToSkip --font sans:20 $(( $i + 1 ))
 	sleep $cameraSleepTime
 }
 
@@ -93,14 +134,19 @@ fi
 # Main part of script
 
 cd ~/$dirToSave
-for i in ${countOfPhotos[*]}
+while [ 1 ]
 do
-  setLightState $PIN_1 $ON
-  sleep $lightSleepTime
-  takePhoto
-  setLightState $PIN_1 $OFF
-  sleep $lightSleepTime
-
+	if [ $(getValue $BUTTON) -eq 1 ]
+	then
+		for i in ${!listOfPins[*]}
+		do
+		setLightState ${listOfPins[$i]} $ON
+  		sleep $lightSleepTime
+  		takePhoto
+		setLightState ${listOfPins[$i]} $OFF
+ 		sleep $lightSleepTime
+		done
+	fi
 done
 
 
